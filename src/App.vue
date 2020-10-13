94a900b4f2b1c7536179273a04f770bd992c0e08
thaisviana <thais@hub9.co>
2020-10-13 12:02:20 -0300

    bootstrap and hello cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <b-row>
+      <h1>{{ title }}</h1>
+    </b-row>
+    <b-row>
+      <HelloWorld msg="hello!" />
+    </b-row>
+    <b-row>
+      <p>{{ parteDoDia() }}</p>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      title: "Locadora de Filmes",
+      horas: new Date().getHours(),
+    };
+  },
+  methods: {
+    parteDoDia() {
+      if (this.horas < 12) {
+        return "Está de dia!";
+      } else if (this.horas >= 12 && this.horas < 18) {
+        return "Está de tarde!";
+      } else if (this.horas >= 18) {
+        return "Está de noite!";
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.row{
+  justify-content: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
