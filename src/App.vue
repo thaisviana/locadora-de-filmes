35cad620f53fde956e8b6f206797baf9ac973697
thaisviana <thais@hub9.co>
2020-10-21 12:03:37 -0300

    movie list

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,33 @@
       <h1>{{ title }}</h1>
     </b-row>
     <b-row>
-      <HelloWorld msg="hello!" />
+      <HelloWorld msg="" />
     </b-row>
     <b-row>
       <p>{{ parteDoDia() }}</p>
+    </b-row>
+    <b-row>
+      <div class="cards">
+         <b-card :key="filme.id" v-for="filme in filmes"
+          :title="filme.titulo"
+          :img-src="filme.imagem"
+          :img-alt="filme.descricao"
+          :id="filme.id"
+          img-top
+          tag="article"
+          style="max-width: 18rem;"
+          class="mb-3 text-center"
+        >
+          <b-card-text>
+            {{ filme.descricao}}
+          </b-card-text>
+          <b-card-text>
+            R$ {{ filme.valor}},00
+          </b-card-text>
+
+          <b-button href="#" block variant="dark"> ALUGAR</b-button>
+        </b-card>
+      </div>
     </b-row>
   </b-container>
 </template>
@@ -27,6 +50,14 @@ export default {
     return {
       title: "Locadora de Filmes",
       horas: new Date().getHours(),
+      filmes : [
+        { id: 1, titulo: "Hulk", descricao: "Filme Ruim", valor: 2, imagem: "https://i.imgur.com/0uthCmp.jpg" },
+        { id: 2, titulo: "Homem de Ferro", descricao: "Homem de Ferro", valor: 10, imagem: "https://i.imgur.com/OA8pDFM.jpg" },
+        { id: 3, titulo: "Thor", descricao: "Bonito ", valor: 20, imagem: "https://i.imgur.com/mt4ZRzw.jpg" },
+        { id: 4, titulo: "Capitão América", descricao: "Um filme de capitão", valor: 40, imagem: "https://i.imgur.com/UFmSVtZ.jpg" },
+        { id: 5, titulo: "Doutor Estranho", descricao: "Magia", valor: 10, imagem: "https://i.imgur.com/pVEDruM.jpg" },
+        { id: 6, titulo: "Pantera Negra", descricao: "Um segundo filme de força", valor: 10, imagem: "https://i.imgur.com/JOSEGKf.jpg" }
+      ]
     };
   },
   methods: {
@@ -44,7 +75,19 @@ export default {
 </script>
 
 <style>
+body{
+  font-family: 'Quicksand', sans-serif;
+  margin-top: 40px;
+}
 .row{
   justify-content: center;
+}
+.cards{
+  display: flex;
+  flex-wrap: inherit;
+  justify-content: space-around;
+}
+.card:hover{
+  transform: scale(1.05);
 }
 </style>
