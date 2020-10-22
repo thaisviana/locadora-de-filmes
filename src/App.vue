08cb2826e95b0e20faff350f4279b2d5bd49a460
thaisviana <thais@hub9.co>
2020-10-22 11:51:33 -0300

    stock

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <HelloWorld msg="" />
     </b-row>
     <b-row>
-      <p>{{ parteDoDia() }}</p>
+      <p> Carrinho: {{ quantidadeNoCarrinho }} filmes </p>
     </b-row>
     <b-row>
       <div class="cards">
@@ -28,7 +28,8 @@
             R$ {{ filme.valor}},00
           </b-card-text>
 
-          <b-button href="#" block variant="dark"> ALUGAR</b-button>
+          <b-button v-if="filme.estoqueDisponivel > 0" href="#" block variant="dark" @click="adicionarAoCarrinho(filme)"> ALUGAR</b-button>
+          <b-button v-else href="#" block variant="danger" disabled> ESGOTADO</b-button>
         </b-card>
       </div>
     </b-row>
@@ -44,19 +45,20 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HelloWorld
   },
   data() {
     return {
       title: "Locadora de Filmes",
       horas: new Date().getHours(),
+      carrinho : [],
       filmes : [
-        { id: 1, titulo: "Hulk", descricao: "Filme Ruim", valor: 2, imagem: "https://i.imgur.com/0uthCmp.jpg" },
-        { id: 2, titulo: "Homem de Ferro", descricao: "Homem de Ferro", valor: 10, imagem: "https://i.imgur.com/OA8pDFM.jpg" },
-        { id: 3, titulo: "Thor", descricao: "Bonito ", valor: 20, imagem: "https://i.imgur.com/mt4ZRzw.jpg" },
-        { id: 4, titulo: "Capitão América", descricao: "Um filme de capitão", valor: 40, imagem: "https://i.imgur.com/UFmSVtZ.jpg" },
-        { id: 5, titulo: "Doutor Estranho", descricao: "Magia", valor: 10, imagem: "https://i.imgur.com/pVEDruM.jpg" },
-        { id: 6, titulo: "Pantera Negra", descricao: "Um segundo filme de força", valor: 10, imagem: "https://i.imgur.com/JOSEGKf.jpg" }
+        { id: 1, estoqueDisponivel : 3, titulo: "Hulk", descricao: "Filme Ruim", valor: 2, imagem: "https://i.imgur.com/0uthCmp.jpg" },
+        { id: 2, estoqueDisponivel : 2, titulo: "Homem de Ferro", descricao: "Homem de Ferro", valor: 10, imagem: "https://i.imgur.com/OA8pDFM.jpg" },
+        { id: 3, estoqueDisponivel : 4, titulo: "Thor", descricao: "Bonito ", valor: 20, imagem: "https://i.imgur.com/mt4ZRzw.jpg" },
+        { id: 4, estoqueDisponivel : 2, titulo: "Capitão América", descricao: "Um filme de capitão", valor: 40, imagem: "https://i.imgur.com/UFmSVtZ.jpg" },
+        { id: 5, estoqueDisponivel : 9, titulo: "Doutor Estranho", descricao: "Magia", valor: 10, imagem: "https://i.imgur.com/pVEDruM.jpg" },
+        { id: 6, estoqueDisponivel : 6, titulo: "Pantera Negra", descricao: "Um segundo filme de força", valor: 10, imagem: "https://i.imgur.com/JOSEGKf.jpg" }
       ]
     };
   },
@@ -70,7 +72,18 @@ export default {
         return "Está de noite!";
       }
     },
+    adicionarAoCarrinho: function(filme) {
+      if (filme.estoqueDisponivel > 0){
+        this.carrinho.push(filme.id)
+        filme.estoqueDisponivel -= 1 
+      }
+    }
   },
+  computed: {
+    quantidadeNoCarrinho: function() {
+      return this.carrinho.length;
+    }
+  }
 };
 </script>
 
